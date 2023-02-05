540539253ed9bf4046abd6ae96e7d991ca72044f
iizmlv <109621932+iizmlv@users.noreply.github.com>
2023-02-05 11:47:42 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+import "./scss/style.scss";
+import "./scss/home.scss";
+import "./scss/header.scss";
+import "./scss/section_one.scss";
+import "./scss/section_two.scss";
+import "./scss/section_three.scss";
+import "./scss/section_four.scss";
+import "./scss/footer.scss";
+import "./scss/test.scss";
+import "./scss/discover.scss";
+import "./scss/header_analog.scss";
+import "./scss/service.scss";
+import "./scss/our_news.scss";
+import "./scss/about_us.scss";
+import "./scss/contact.scss";
